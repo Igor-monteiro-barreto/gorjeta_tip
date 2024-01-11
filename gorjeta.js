2c59f5f3803e35bcc3a6b3de709d419314e61767
Igor-monteiro-barreto <igormonteirobarreto047@gmail.com>
2024-01-11 19:28:28 -0300

    esta funcionando

--- gorjeta.js
+++ gorjeta.js
@@ -2,7 +2,7 @@
 function Calcular(){
 let valcont = document.getElementById('iconta').value
 let res = document.getElementById('res')
-if(valcont <= 0){
+if(valcont === "" || isNaN(valcont) || valcont < 0){
     window.alert('Algum componente esta vazio tente novamente');
 }
 else{
@@ -11,10 +11,14 @@ else{
     let valorTotal = parseFloat(valcont) + gorjeta;
 
     
-    document.getElementById('tgorjeta').value = gorjeta.toFixed(2);
-    document.getElementById('vt').value = valorTotal.toFixed(2);
+    document.getElementById('tgorjeta').value = ('$' + gorjeta.toFixed(2));
+    document.getElementById('vt').value = ('$' + valorTotal.toFixed(2));
+    
     res.hidden = false
+    
 }
 }
-
-
+function porcem(){
+    let rangevalue = document.getElementById('tipInput').value
+    document.getElementById('porcentagemDisplay').innerText = rangevalue + '%';
+}
