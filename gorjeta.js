7ff407a98d560cc535c9f6abbcbd4e1b2423bd4e
Igor-monteiro-barreto <igormonteirobarreto047@gmail.com>
2024-01-11 16:59:37 -0300

    Quase pronto

--- gorjeta.js
+++ gorjeta.js
@@ -1,5 +1,20 @@
 
 function Calcular(){
-    let valcont = document.querySelector("#iconta")
-window.alert(Number.valcont , "Deu certo kkk");
+let valcont = document.getElementById('iconta').value
+let res = document.getElementById('res')
+if(valcont <= 0){
+    window.alert('Algum componente esta vazio tente novamente');
 }
+else{
+    let porcentagem = document.getElementById('tipInput').value;
+    let gorjeta = (valcont * porcentagem) / 100;
+    let valorTotal = parseFloat(valcont) + gorjeta;
+
+    
+    document.getElementById('tgorjeta').value = gorjeta.toFixed(2);
+    document.getElementById('vt').value = valorTotal.toFixed(2);
+    res.hidden = false
+}
+}
+
+
